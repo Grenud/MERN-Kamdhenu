b1017d2e180af217edd7e38b9711a22a410396d5
Vicky Kumar <vickykrsingh27@gmail.com>
2024-07-22 12:54:05 +0530

    added serverless function for vercel deployment

--- server/app.js
+++ server/app.js
@@ -1,19 +1,6 @@
-import express from 'express'
-import dontenv from 'dotenv'
-import cattleRoute from './routes/cattle.routes.js'
-import cors from 'cors'
-dontenv.config()
-const app = express()
+import app from "./api/index.js";
 const PORT = process.env.PORT;
 
-app.use(cors())
-app.use('/api/v1/cattle',cattleRoute)
-app.use('/',(req,res)=>{
-    res.status(200).json({
-        success:true,
-        message:"Server is responding...."
-    })
-})
 
 
 app.listen(PORT,()=>{
