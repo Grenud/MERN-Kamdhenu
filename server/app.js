a8c193e7331ebcad7b1d268412472ab05f3d011e
Vicky Kumar <vickykrsingh27@gmail.com>
2024-07-22 10:08:32 +0530

    setup for vercel deployment

--- server/app.js
+++ server/app.js
@@ -3,16 +3,21 @@ import dontenv from 'dotenv'
 import cattleRoute from './routes/cattle.routes.js'
 import cors from 'cors'
 dontenv.config()
-const app = express()
+export const app = express()
 const PORT = process.env.PORT;
 
 app.use(cors())
 app.use('/api/v1/cattle',cattleRoute)
 app.use('/',(req,res)=>{
-    res.send("server is responding....")
+    res.status(200).json({
+        success:true,
+        message:"Server is responding...."
+    })
 })
 
 
 app.listen(PORT,()=>{
     console.log(`server is running on the port ${PORT}..............`)
 })
+
+
