66987f810fe41743c888dd5f6df7bef74435a5bf
vickykrsingh <vickykrsingh27@gmail.com>
2024-09-05 16:04:03 +0530

    removed server folder

--- server/routes/cattle.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getCattle, getCattleById } from "../controllers/cattle.js";
-const router = Router();
-
-router.get("/get-cattle", getCattle);
-router.get("/get-cattle/:id",getCattleById);
-
-export default router;
