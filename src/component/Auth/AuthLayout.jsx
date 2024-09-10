1c9665732ce1bf0432f2f369cf0fe1b88cdf75dd
vickykrsingh <vickykrsingh27@gmail.com>
2024-09-11 02:04:29 +0530

    modified authLayout.jsx

--- src/component/Auth/AuthLayout.jsx
+++ src/component/Auth/AuthLayout.jsx
@@ -1,37 +1,39 @@
-console.log('authLayout is running')
 import axios from 'axios';
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { SetUser } from '../../redux/AuthSlice';
-import { useNavigate, useLocation } from 'react-router-dom';  // import useLocation
-console.log('hello world')
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function AuthLayout({ children }) {
     const navigate = useNavigate();
     const location = useLocation();  // get the current location
     const dispatch = useDispatch();
+
     const fetchUserData = async () => {
-        console.log('running fetch user data')
+        console.log('Fetching user data...');
         try {
             const response = await axios.get("/api/auth/google-user-data");
-            console.log('response is ------ ',response)
+            console.log('response is ------ ', response);
             if (response.data.success) {
                 dispatch(SetUser({
                     user: response.data.user
                 }));
+            } else {
+                // Handle case when the response is not successful
+                navigate('/login');
             }
         } catch (error) {
+            console.log('Error fetching user data: ', error);
             dispatch(SetUser({
                 user: null
             }));
-            console.log(error)
             navigate('/login');
         }
     };
 
     useEffect(() => {
-        fetchUserData();
-    }, [location.pathname, dispatch]);  // Add location.pathname as a dependency
+        fetchUserData(); // Fetch data when component mounts
+    }, []);  // Empty array ensures this only runs on component mount
 
     return (
         <>
