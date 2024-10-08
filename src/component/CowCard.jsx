d7337afac8b6d35c5b4855ed29e7590841f5c0e0
vickykrsingh <vickykrsingh27@gmail.com>
2024-10-08 16:10:24 +0530

    Enhance the dynamic page using useCallback,useMemo,lazy imports and lazy loading and structure the code

--- src/component/CowCard.jsx
+++ src/component/CowCard.jsx
@@ -3,31 +3,36 @@ function CowCard({ imgSrc, name }) {
   const cleanName = name.replace(/\b[A-Z]{1,2}\d{1,2}\b/, '').trim() + " Gaumata";
 
   return (
-    <section className="bg-green-800 text-white rounded-lg overflow-hidden p-4 flex flex-col gap-4">
+    <section className="bg-gray-100 text-black border-2 shadow-md rounded-lg overflow-hidden flex flex-col gap-2">
       <figure className="overflow-hidden">
         <img
           src={imgSrc}
           alt={`Image of ${name}`} // Dynamic alt text for accessibility
-          className="hover:scale-105 duration-300"
+          className="hover:scale-105 duration-300 h-72 object-cover w-full"
         />
       </figure>
-      <h2 className="text-lg font-semibold tracking-wider">{name}</h2>
-      <h2 className="font-semibold tracking-wide text-xl">
-        Help us care for {cleanName}
-      </h2>
-      <p className="text-sm tracking-wide">
-        Support and reward options for our sponsoring partners:
-        <br />
-        <strong>WHITE:</strong> Custom amount
-        <br />
-        <strong>GREEN:</strong> 1 month
-        <br />
-        <strong>BRONZE:</strong> 3 months
-        <br />
-        <strong>SILVER:</strong> 6 months
-        <br />
-        <strong>GOLD:</strong> 12 months
-      </p>
+      <div className="pl-3 flex flex-col gap-1 pb-2">
+        <h2 className="text-lg font-semibold tracking-wider">{name}</h2>
+        <h2 className="font-semibold tracking-wide text-xl">
+          Help us care for {cleanName}
+        </h2>
+        <p className="text-sm tracking-wide">
+          Support and reward options for our sponsoring partners:
+        </p>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-2">
+          <div className="flex gap-2 items-center"><div className="w-3 h-3 rounded-full bg-white" ></div> Custom amount</div>
+          <div className="flex gap-2 items-center"><div className="w-3 h-3 rounded-full bg-green-400"></div> 1 month
+          </div>
+          <div className="flex gap-2 items-center"><div className="w-3 h-3 rounded-full bg-[#CD7F32]"></div> 3 months
+          </div>
+          <div className="flex gap-2 items-center">
+            <div className="w-3 h-3 rounded-full bg-[#C0c0c0]" ></div> 6 months
+          </div>
+          <div className="flex gap-2 items-center">
+            <div className="w-3 h-3 rounded-full bg-[#FFD700]"></div> 12 months
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
